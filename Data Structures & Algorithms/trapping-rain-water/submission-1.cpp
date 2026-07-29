559f06c12bf99e6ece79257f09aeb7f25363149a
Priyanshi Priya <155705705+Priyanshi04priya@users.noreply.github.com>
2026-07-29 16:10:50 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,39 @@
+// class Solution {
+// public:
+//     int trap(vector<int>& ht) {
+//         int n=ht.size();
+//         vector<int> lmax(n), rmax(n);
+//         lmax[0]=ht[0], rmax[n-1]=ht[n-1];
+//         for(int i=1;i<n;i++){
+//             lmax[i]=max(ht[i],lmax[i-1]);
+//         }
+//         for(int j=n-2;j>=0;j--){
+//             rmax[j]=max(ht[j], rmax[j+1]);
+//         }
+//         int ans=0;
+//         for(int i=0;i<n;i++){
+//             ans+=min(lmax[i],rmax[i])-ht[i];
+//         }
+//         return ans;
+//     }
+// };
+
+class Solution {
+public:
+    int trap(vector<int>& ht) {
+        int n=ht.size();
+        vector<int> lmax(n), rmax(n);
+        lmax[0]=ht[0], rmax[n-1]=ht[n-1];
+        for(int j=n-2;j>=0;j--){
+            rmax[j]=max(ht[j], rmax[j+1]);
+        }
+
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(i>0)lmax[i]=max(ht[i],lmax[i-1]);
+            ans+=min(lmax[i],rmax[i])-ht[i];
+        }
+        return ans;
+    }
+};
+
