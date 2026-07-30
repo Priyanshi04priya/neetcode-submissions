c3b71c0ff932beeb34d79a3f981886f5fbbcff65
Priyanshi Priya <155705705+Priyanshi04priya@users.noreply.github.com>
2026-07-30 12:13:25 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        int n=intervals.size();
+        if(n==0)return true;
+        sort(intervals.begin(), intervals.end(),[](const Interval& a, const Interval& b) {
+                 return a.start < b.start;
+        });
+
+        //normal sorting nhi kr sakte bcz its a class not vector
+        for(int i=1;i<n;i++){
+            if(intervals[i].start<intervals[i-1].end)return false;
+        }
+        return true;
+    }
+};
