29bf256f3fa59c8963e1bccc762fa0458eb65d01
Priyanshi Priya <155705705+Priyanshi04priya@users.noreply.github.com>
2026-07-31 17:59:27 +0530

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.cpp
@@ -0,0 +1,68 @@
+// class Solution {
+// public:
+//     int f(int i,vector<int>& nums,vector<int>& dp){
+//         if(i<0)return 0;
+//         if(i==0)return nums[0];
+//         if(dp[i]!=-1)return dp[i];
+//         int take=nums[i]+f(i-2, nums, dp);
+//         int ntake=f(i-1,nums, dp);
+//         return dp[i]=max(take, ntake);
+//     }
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> dp(n,-1);
+//         return f(n-1,nums,dp);
+//     }
+// };
+
+
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> dp(n+1);
+//         if (n==1) return nums[0];
+//         dp[0]=nums[0];
+//         dp[1]=max(nums[0], nums[1]);
+//         for(int i=2;i<n;i++){
+//             int take=nums[i]+dp[i-2];
+//             int ntake=dp[i-1];
+//             dp[i]=max(take, ntake);
+//         }
+//         return dp[n-1];
+//     }
+// };
+
+// class Solution {
+// public:
+//     int f(int i,vector<int>& nums,vector<int>& dp){
+//         if(i<0)return 0;
+//         if(i==0)return nums[0];
+//         if(dp[i]!=-1)return dp[i];
+//         int take=nums[i]+f(i-2, nums, dp);
+//         int ntake=f(i-1,nums, dp);
+//         return dp[i]=max(take, ntake);
+//     }
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<int> dp(n,-1);
+//         return f(n-1,nums,dp);
+//     }
+// };
+
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if (n==1) return nums[0];
+        int prev1=max(nums[0], nums[1]);
+        int prev2=nums[0];
+        for(int i=2;i<n;i++){
+            int curr=max(prev1, nums[i]+prev2);
+            prev2=prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+};
