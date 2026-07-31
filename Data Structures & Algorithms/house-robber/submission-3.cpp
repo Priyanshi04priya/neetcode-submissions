b44c2647ec9be160352d6dd90c3a850568df26de
Priyanshi Priya <155705705+Priyanshi04priya@users.noreply.github.com>
2026-07-31 17:41:33 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int f(int i,vector<int>& nums,vector<int>& dp){
+        if(i<0)return 0;
+        if(i==0)return nums[0];
+        if(dp[i]!=-1)return dp[i];
+        int take=nums[i]+f(i-2, nums, dp);
+        int ntake=f(i-1,nums, dp);
+        return dp[i]=max(take, ntake);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return f(n-1,nums,dp);
+    }
+};
