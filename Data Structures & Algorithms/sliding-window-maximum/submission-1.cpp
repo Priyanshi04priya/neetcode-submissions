0d7c90726055d83066888f8f5ba6b546bfb1b1af
Priyanshi Priya <155705705+Priyanshi04priya@users.noreply.github.com>
2026-07-31 12:26:40 +0530

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        //we will store index to have a check of out of bound condition 
+        //remove all elements from front at index less than equal to (i-k)
+        //deque me start me max rakhenge agar next element greater hai to phle wale min ko pop karate jao from back
+        //push krdo
+        //jb bhi i>=k-1 to max ko ans me store karo
+        int n=nums.size();
+        deque<int> dq;
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            while(!dq.empty() && dq.front()<=i-k)dq.pop_front();
+            while(!dq.empty() && nums[dq.back()]<=nums[i])dq.pop_back();
+            dq.push_back(i);
+            if(i>=k-1)ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
